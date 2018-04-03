f2c446d3a2962ae5fef5e57cff4b00c1be3853dc
Unknown <chitzinwin@gmail.com>
2018-04-02 20:09:30 -0400

    resourcemergeddays is latest

---
+++ init.php
@@ -0,0 +1,140 @@
+<?
+ini_set('session.gc_maxlifetime', 3600);
+session_set_cookie_params(3600);
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // $entityBody = file_get_contents('php://input');
+    // echo $entityBody;
+    if(isset($_POST["filename"]) && isset($_POST["value"]) ){
+         $_SESSION["xlsx"] = $_POST["value"];
+         echo  file_exists($_SESSION["xlsx"]);    //final confirmation the file exsitence
+        exit;
+    }
+    
+    echo false; exit;
+    
+}
+
+
+$files = glob('spreadsheets/*.xlsx');
+//  print(json_encode($files));
+$json = new stdClass();
+ foreach($files as $file){
+   $name = basename($file, '.xlsx');
+    $json->$name =$file;
+ }
+
+// print json_encode($json);
+// print $json->F18;
+ $jsonfromPHP = json_encode($json);
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+<script type="text/javascript" src="schedulejs/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="schedulejs/jquery-ui.min.js"></script>
+<link rel="stylesheet" media="screen" href="schedulejs/jquery-ui.css"/>
+
+
+<script>
+var files = <?php echo $jsonfromPHP; ?>;   //array of filename with path
+
+var buttons = new Object();
+    $.each(files, (key, value)=>{	
+        buttons[key]= function () {
+
+                            $.ajax({
+                            type: 'post',
+                            url: 'init.php',
+                            data: {filename: key, value : value},
+                            success: function(data) {
+                                console.log(data);
+                            if(data){
+                            location.replace("resourcemergeddays.php");      //only file selection is successful switch to next page
+                             }
+                                 }                     
+                            
+
+                        });
+              }
+}
+);
+
+// buttons[buttonsength]= {
+//       text: "Ok",
+//       icons: {
+//         primary: "ui-icon-heart"
+//       },
+//       click: function() {
+//         $( this ).dialog( "close" );
+//       }
+ 
+//       // Uncommenting the following line would hide the text,
+//       // resulting in the label being used as a tooltip
+//       //showText: false
+//     }
+
+console.log(files);
+
+$( function() {
+    var dialog =  $( "#dialog" ).dialog({
+        autoOpen: false,
+        buttons : buttons
+    });
+    window.onload = ()=>{
+        dialog.dialog('open');
+    }
+    
+    
+
+    
+ } );
+
+
+ 
+
+        
+</script>
+<style>
+
+    .ui-dialog .ui-dialog-buttonpane .ui-dialog-buttonset
+    {
+        float:none; 
+
+    }
+    .ui-dialog .ui-dialog-buttonpane
+    {
+        text-align:center;
+         vertical-align:bottom;
+      line-height: 3.5em;
+      display: flex;
+  justify-content: center;
+  align-items: center;
+    }
+ 
+ .ui-button{
+     display: block;
+ }
+
+
+.ui-dialog-content::-webkit-scrollbar {
+  -webkit-appearance: none;
+  width: 11px;
+  height: 11px;
+}
+
+.ui-dialog-content::-webkit-scrollbar-thumb {
+  border-radius: 8px;
+  border: 2px solid white; /* should match background, can't be transparent */
+  background-color: rgba(0, 0, 0, .5);
+}
+</style>
+</head>
+<body>
+<div id="dialog" title="File(Xlsx) found in directory"></div>
+</body>
+
+</html>
